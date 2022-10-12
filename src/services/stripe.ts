f6344b49b9dd9f1d81911a37b6497578f588ef67
lucianobs1 <lucianotetsu@gmail.com>
2022-10-12 07:30:21 -0300

    create account stripe and register information in faunadb

--- src/services/stripe.ts
+++ src/services/stripe.ts
@@ -1,9 +1,9 @@
-import Stripe from "stripe";
+import Stripe from 'stripe';
 
 export const stripe = new Stripe(process.env.STRIPE_API_KEY, {
-  apiVersion: "2022-08-01",
+  apiVersion: '2022-08-01',
   appInfo: {
-    name: "ignews",
-    version: "0.1.0",
+    name: 'ignews',
+    version: '0.1.0',
   },
 });
