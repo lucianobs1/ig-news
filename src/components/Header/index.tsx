44ab88a1deb3286e36585d0888856b28c6b08724
lucianobs1 <lucianotetsu@gmail.com>
2022-10-18 11:35:42 -0300

    create ActiveLink

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,25 @@
-import { SignInButton } from "../SignInButton";
-import styles from "./styles.module.scss";
+import { SignInButton } from '../SignInButton';
+import styles from './styles.module.scss';
+import Image from 'next/image';
+import { ActiveLink } from '../ActiveLink';
 
 export function Header() {
   return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
-        <img src="/images/logo.svg" alt="ig-news" />
+        <Image
+          height={100}
+          width={100}
+          src="/images/logo.svg"
+          alt="ig-news"
+        />
         <nav>
-          <a className={styles.active}>Home</a>
-          <a>Posts</a>
+          <ActiveLink activeClassName={styles.active} href="/">
+            <a className={styles.active}>Home</a>
+          </ActiveLink>
+          <ActiveLink activeClassName={styles.active} href="/posts">
+            <a>Posts</a>
+          </ActiveLink>
         </nav>
 
         <SignInButton />
